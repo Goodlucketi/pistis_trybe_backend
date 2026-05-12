cec0bf4314b44fc34901cf221db5f58b8cac031e
Goodlucketi <goodlucketim94@gmail.com>
2026-05-12 14:20:32 +0100

    notes feature

--- src/routers.ts
+++ src/routers.ts
@@ -5,6 +5,7 @@ import postsV1Router from "./posts/posts.routes";
 import groupsV1Router from "./groups/groups.routes";
 import followersV1Router from "./followers/followers.routes";
 import chatsV1Router from "./chats/chats.routes";
+import notesv1Router from "./notes/notes.routes";
 
 const pistisTribeRouterV1 = Router();
 
@@ -14,5 +15,6 @@ pistisTribeRouterV1.use(postsV1Router);
 pistisTribeRouterV1.use(groupsV1Router);
 pistisTribeRouterV1.use(followersV1Router);
 pistisTribeRouterV1.use(chatsV1Router);
+pistisTribeRouterV1.use(notesv1Router);
 
 export { pistisTribeRouterV1 };
