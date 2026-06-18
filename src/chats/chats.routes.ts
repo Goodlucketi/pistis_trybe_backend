a53d37cb975d94606ce7bbbea90e154b2a6b2b92
Jesuk Global <goodlucketim94@gmail.com>
2026-06-18 01:00:01 +0100

    Direct chat Bugs

--- src/chats/chats.routes.ts
+++ src/chats/chats.routes.ts
@@ -13,7 +13,7 @@ import { uploadSingle } from "../middlewares/upload";
 const chatsV1Router = Router();
 
 chatsV1Router.get("/chats", getChats);
-chatsV1Router.post("/chats", startDirectChat);
+chatsV1Router.post("/chats/direct", startDirectChat);
 chatsV1Router.post("/chats/groups", createGroupChat);
 chatsV1Router.get("/chats/:id/messages", getMessages);
 chatsV1Router.post("/chats/:id/messages", uploadSingle, sendMessage);
