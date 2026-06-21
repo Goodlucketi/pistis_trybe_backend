79c0801572978fc7e2cbd0a5334376227c462812
Jesuk Global <goodlucketim94@gmail.com>
2026-06-22 00:55:33 +0100

    devotional features

--- src/routers.ts
+++ src/routers.ts
@@ -8,6 +8,7 @@ import notesv1Router from "./notes/notes.routes";
 import commentsV1Router from "./comments/comments.routes";
 import notificationsV1Router from "./notifications/notifications.routes";
 import adminV1Router from "./admin/admin.routes";
+import devotionalsV1Router from "./devotionals/devotionals.routes";
 
 const pistisTribeRouterV1 = Router();
 
@@ -20,5 +21,6 @@ pistisTribeRouterV1.use(notesv1Router);
 pistisTribeRouterV1.use(commentsV1Router);
 pistisTribeRouterV1.use(notificationsV1Router);
 pistisTribeRouterV1.use(adminV1Router);
+pistisTribeRouterV1.use(devotionalsV1Router);
 
 export { pistisTribeRouterV1 };
